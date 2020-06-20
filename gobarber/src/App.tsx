040f3120b02ab9fee5b1fc46ce650a74a10a1b10
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-20 19:05:03 -0300

    form validation on sigin page

--- gobarber/src/App.tsx
+++ gobarber/src/App.tsx
@@ -10,7 +10,7 @@ import SignUp from './Pages/SingUp';
 const App: React.FC = () => {
   return (
     <>
-      <SignUp />
+      <SignIn />
       <GlobalStyle />
     </>
   );
--- gobarber/src/Pages/SingIn/index.tsx
+++ gobarber/src/Pages/SingIn/index.tsx
@@ -1,6 +1,10 @@
 /* Import libs */
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 /* Import CSS */
 import { Container, Content, Background } from './styles';
@@ -13,11 +17,33 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Logo GoBarber" />
-        <form>
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
           <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
           <Input
@@ -28,7 +54,8 @@ const SignIn: React.FC = () => {
           />
           <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
+
         <a href="/rota">
           <FiLogIn />
           Criar Conta
